f525ee839fd28d16f521b8aabd769d6ca090d6db
user638726 <lienjackson35@gmail.com>
2025-07-31 03:51:41 +0800

    shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue(), VueRouter()],
   resolve: {
     alias: {
