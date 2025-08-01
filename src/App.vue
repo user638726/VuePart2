cc6f9d22c26dbab81bdbcd169283c096741d660b
user638726 <lienjackson35@gmail.com>
2025-08-02 02:04:59 +0800

    ＂加＂加table＂

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import TopNavbar from '@/components/ui/Layout/TopNavbar.vue'
-import Sidebar from '@/components/ui/Layout/Sidebar.vue'
+import AuthLayout from '@/components/ui/Layout/main/AuthLayout.vue'
 </script>
 
 <template>
-  <Sidebar />
-  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavbar />
-    <main>
-      <RouterView />
-    </main>
-  </div>
+  <AuthLayout>
+    <RouterView />
+  </AuthLayout>
 </template>
--- src/components/ui/Layout/Sidebar.vue
+++ src/components/ui/Layout/Sidebar.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import SidebarLinks from '@/components/ui/Layout/SidebarLinks.vue'
+import  Button  from '@/components/ui/button/Button.vue'
 const links = [
   {
     title: 'Dashboard',
@@ -42,13 +43,13 @@ const accountlinks = [
     class="flex flex-col h-screen gap-2 border-r fixed bg-muted/40 lg:w-52 w-16 transition-[width]"
   >
     <div class="flex items-center justify-between h-16 gap-1 px-2 border-b lg:px-4 shrink-0">
-      <button variant="outline" size="icon" class="w-8 h-8">
+      <Button variant="outline" size="icon" class="w-8 h-8">
         <iconify-icon icon="lucide:menu"></iconify-icon>
-      </button>
+      </Button>
 
-      <button variant="outline" size="icon" class="w-8 h-8">
+      <Button variant="outline" size="icon" class="w-8 h-8">
         <iconify-icon icon="lucide:plus"></iconify-icon>
-      </button>
+      </Button>
     </div>
 
     <nav class="relative flex flex-col justify-between h-full gap-2">
