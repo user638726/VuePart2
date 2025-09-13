b6feabc54763e2b1609961c02e0d479042e6d553
user638726 <lienjackson35@gmail.com>
2025-09-14 00:05:16 +0800

    格式化

--- src/components/ui/AppInPlaceEdit/AppInPlaceEditStatus.vue
+++ src/components/ui/AppInPlaceEdit/AppInPlaceEditStatus.vue
@@ -4,34 +4,31 @@ const value = defineModel<'in-progress' | 'completed'>()
 const emit = defineEmits(['commit'])
 
 const { readonly = false } = defineProps<{
-  readonly?:boolean
+  readonly?: boolean
 }>()
 
-const toggleValue = () =>{
-  if(readonly) return
+const toggleValue = () => {
+  if (readonly) return
 
   value.value = value.value === 'completed' ? 'in-progress' : 'completed'
   emit('commit')
-
-
 }
-
 </script>
 <template>
-   <div class="text-2xl cursor-pointer" @click="toggleValue">
-     <Transition mode="out-in">
-
-     <iconify-icon v-if="value === 'completed'" icon="lucide:circle-check" class="text-green-500" />
-     <iconify-icon v-else icon="lucide:circle-dot" class="text-gray-500" />
-
-     </Transition>
-
-   </div>
-
+  <div class="text-2xl cursor-pointer" @click="toggleValue">
+    <Transition mode="out-in">
+      <iconify-icon
+        v-if="value === 'completed'"
+        icon="lucide:circle-check"
+        class="text-green-500"
+      />
+      <iconify-icon v-else icon="lucide:circle-dot" class="text-gray-500" />
+    </Transition>
+  </div>
 </template>
 <style scoped>
 .v-enter-active,
-.v-leave-active{
+.v-leave-active {
   transition: transform 0.1s;
 }
 
@@ -39,6 +36,4 @@ const toggleValue = () =>{
 .v-leave-to {
   transform: scale(0.3);
 }
-
-
 </style>
